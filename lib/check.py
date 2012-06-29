e5869814c58fbc79414c7ebb4d1b32e05661eb80
Sam Simmons <sam@samiconductor.com>
2012-06-29 00:16:21 -0500

    added ip check and records update methods

---
+++ lib/check.py
@@ -0,0 +1,33 @@
+import re
+
+import requests
+
+
+cached = None
+CHECK_IP_URL = 'http://checkip.dyndns.com'
+
+
+class CheckIpRequestException(requests.RequestException):
+    pass
+
+
+class CheckIpParseException(Exception):
+    pass
+
+
+def check_ip():
+    global cached
+    if cached:
+        return cached
+
+    try:
+        r = requests.get(CHECK_IP_URL)
+    except requests.RequestException as e:
+        raise CheckIpRequestException(e.message)
+
+    s = re.search('(\d{1,3}\.?){4}', r.content)
+    if s:
+        cached = s.group()
+        return cached
+
+    raise CheckIpParseException('no ip address found in response {}'.format(r.content))
--- namecheap.py
+++ namecheap.py
@@ -1,6 +1,5 @@
-import requests
-
 from lib.config import read_config
+from lib.update import update_records
 
 
 if __name__ == '__main__':
@@ -10,13 +9,4 @@ if __name__ == '__main__':
     domains = read_config(config_file, search_path, kill=True)
 
     for domain in domains:
-        for host in domain.hosts:
-            try:
-                r = requests.get('https://dynamicdns.park-your-domain.com/update', params={
-                    'domain': domain.name,
-                    'host': host,
-                    'password': domain.password,
-                    'ip': domain.ip
-                }, timeout=1)
-            except requests.ConnectionError as e:
-                print e.message
+        update_records(domain)
